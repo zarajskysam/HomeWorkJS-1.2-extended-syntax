ab8ea13bb3cf3c2d19ae8f66767bf6ba0767b4a1
Semyon Zarajsky <zarajksysam92@gmail.com>
2020-06-16 14:33:26 +0300

    task corrections

--- task.js
+++ task.js
@@ -1,14 +1,11 @@
 function getResult(a,b,c){
     let discriminant = Math.pow(b, 2) - 4 * a * c;
-    let x;
+    let x = [];
     if (discriminant > 0) {
         x = [(-b + Math.sqrt(discriminant)) / (2 * a), (-b - Math.sqrt(discriminant)) / (2 * a)];
     } else if (discriminant === 0) {
         x = [-b / 2 *a];
-    } else {
-        x = [];
-    }
-
+    } 
     return x;
 }
 
@@ -16,23 +13,16 @@ function getAverageMark(marks){
     // код для задачи №2 писать здесь
     let averageMark;
     if (marks.length === 0) {
-        averageMark = 0;
+        return 0;
     } else if (marks.length > 5) {
         console.log('Количество оценок больше пяти');
-        marks.splice(5);
-        let summarks = 0;
-        for (let i = 0; i < marks.length; i++) {
-            summarks += marks[i];
-        }
-        averageMark = summarks / marks.length;
-    } else if (marks.length <= 5) {
-        let summarks = 0;
-        for (let i = 0; i < marks.length; i++) {
-            summarks += marks[i];
-        }
-        averageMark = summarks / marks.length;
+        marks.splice(5);   
     }
-
+    let summarks = 0;
+    for (let i = 0; i < marks.length; i++) {
+        summarks += marks[i];
+    }
+    averageMark = summarks / marks.length;
     return averageMark;
 }
 
@@ -41,10 +31,8 @@ function askDrink(name,dateOfBirthday){
     let result;
     let years = new Date().getFullYear() - dateOfBirthday.getFullYear();
     if (years >= 18) {
-        result = `Не желаете ли олд-фэшн, ${name}?`;
+        return (`Не желаете ли олд-фэшн, ${name}?`);
     } else if (years < 18) {
-        result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+        return (`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
     }
-
-    return result;
 }
